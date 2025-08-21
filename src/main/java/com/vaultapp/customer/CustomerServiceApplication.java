d7a2a84bc84b88abfcb7af8b97564f56d248d779
Ankit Khandelwal <ankit.khandelwal@hashicorp.com>
2025-08-21 19:05:45 +0530

    With Sringboot config to fetch the creads from Vault

--- src/main/java/com/vaultapp/customer/CustomerServiceApplication.java
+++ src/main/java/com/vaultapp/customer/CustomerServiceApplication.java
@@ -1,14 +1,7 @@
 package com.vaultapp.customer;
 
-import javax.sql.DataSource;
-
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
